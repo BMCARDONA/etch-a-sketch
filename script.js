c6d0013e44f13d39f1e06e186d3b0c2adbc5e4b2
Bradley Cardona <bradleymathematics@gmail.com>
2022-06-14 17:33:25 -0400

    create makeCell()
    The makecell() function creates the number of cells equal to the number of squares that will be in the grid. Said number is prompted to the user in the function gridSize().

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
 
+const container = document.querySelector('#container');
+function makeCell (num) {
+    for (let i = 0; i < num; i++) {
+        for (let j = 0; j < num; j++) {
+            let cell = document.createElement('div');
+            cell.classList.add('cell');
+            container.appendChild(cell);
+        }
+    }
+}
+
+
 function gridSize() {
   let number = prompt("How many squares per side? (Maximum: 69)", 10); 
    while (number > 69) {
