c06f312d6145e1a30f5cb06d917baa4a72bd6315
Bradley Cardona <bradleymathematics@gmail.com>
2022-06-23 22:33:08 -0400

    Add slider
    The slider allows one to easily change the size of the grid. This part took me 3 hours to figure out -- no regrets!

--- script.js
+++ script.js
@@ -1,105 +1,81 @@
 
-const container = document.querySelector('#container');
-function makeCell (num) {
-    for (let i = 0; i < num; i++) {
-        for (let j = 0; j < num; j++) {
-            let cell = document.createElement('div');
-            cell.classList.add('cell');
-            container.appendChild(cell);
-        }
+  const container = document.querySelector('#container');
+  function makeCell (num) {
+      for (let i = 0; i < num; i++) {
+          for (let j = 0; j < num; j++) {
+              let cell = document.createElement('div');
+              cell.classList.add('cell');
+              container.appendChild(cell);
+          }
+      }
+  }
+
+  function makeGrid(numRows, numCols) {
+      container.style.setProperty('--numRows', numRows); 
+      container.style.setProperty('--numCols', numCols); 
+  }
+
+  let price = document.querySelector('#price');
+  let output = document.querySelector('.price-output');
+  let number = price.value
+  output.textContent = price.value;
+
+  price.addEventListener('input', function() {
+    output.textContent = price.value;
+    // This code is just adding extra cells -- that's not what we want
+    while (container.firstChild) {
+      container.removeChild(container.firstChild);
     }
-}
+    number = price.value 
+    makeCell(number);
+    makeGrid(number, number);
+  });
 
-function makeGrid(numRows, numCols) {
-    container.style.setProperty('--numRows', numRows); 
-    container.style.setProperty('--numCols', numCols); 
-}
+  makeCell(number);
+  makeGrid(number, number);
 
-let gridLength = 0;
-function gridSize() {
-  let number = prompt("How many squares per side? (Maximum: 69)", 10); 
-   while (number > 69) {
-       number = prompt("Please enter a number less than or equal to 69!");
-   }
-   gridLength += number;
-   makeCell(number);
-   makeGrid(number, number);
-}
 
-gridSize();
-
-// black button
-function changeColorBlack (target) {
-  target.style.backgroundColor = 'black';
-}
-container.addEventListener("mouseover", function (e) {
-  let target = e.target;
-  if (target.matches('.cell')) {
-      changeColorBlack(target);
+  // black button
+  function changeColorBlack (target) {
+    target.style.backgroundColor = 'black';
   }
-});
-const blackBtn = document.querySelector('.blackBtn');
-blackBtn.addEventListener("click", function (e) {
   container.addEventListener("mouseover", function (e) {
-      let target = e.target;
-      if (target.matches('.cell')) {
-          changeColorBlack(target);
-      }
+    let target = e.target;
+    if (target.matches('.cell')) {
+        changeColorBlack(target);
+    }
   });
-}); 
+  const blackBtn = document.querySelector('.blackBtn');
+  blackBtn.addEventListener("click", function (e) {
+    container.addEventListener("mouseover", function (e) {
+        let target = e.target;
+        if (target.matches('.cell')) {
+            changeColorBlack(target);
+        }
+    });
+  }); 
 
-// reset button
-const reset = document.querySelector('.resetBtn');
-reset.addEventListener('click', function() {
-     window.location.reload();
-});
-
-
-// rainbow button
-function changeColorRandom (target) {
-  var x = Math.floor(Math.random() * 256);
-  var y = Math.floor(Math.random() * 256);
-  var z = Math.floor(Math.random() * 256);
-  var bgColor = "rgb(" + x + "," + y + "," + z + ")";
-  target.style.backgroundColor = bgColor;
-}
-const rainbowBtn = document.querySelector('.rainbowBtn');
-rainbowBtn.addEventListener("click", function (e) {
-  container.addEventListener("mouseover", function (e) {
-      let target = e.target;
-      if (target.matches('.cell')) {
-          changeColorRandom(target);
-      }
+  // reset button
+  const reset = document.querySelector('.resetBtn');
+  reset.addEventListener('click', function() {
+      window.location.reload();
   });
+
+
+  // rainbow button
+  function changeColorRandom (target) {
+    var x = Math.floor(Math.random() * 256);
+    var y = Math.floor(Math.random() * 256);
+    var z = Math.floor(Math.random() * 256);
+    var bgColor = "rgb(" + x + "," + y + "," + z + ")";
+    target.style.backgroundColor = bgColor;
+  }
+  const rainbowBtn = document.querySelector('.rainbowBtn');
+  rainbowBtn.addEventListener("click", function (e) {
+    container.addEventListener("mouseover", function (e) {
+        let target = e.target;
+        if (target.matches('.cell')) {
+            changeColorRandom(target);
+        }
+    });
 });  
-
-
-
-// eraser button
-function changeColorWhite (target) {
-  target.style.backgroundColor = 'white';
-}
-const eraseBtn = document.querySelector('.eraseBtn');
-eraseBtn.addEventListener("click", function (e) {
-  container.addEventListener("mouseover", function (e) {
-      let target = e.target;
-      if (target.matches('.cell')) {
-          changeColorWhite(target);
-      }
-  });
-});  git 
-
-
-// clear button
-
-function clearGrid() {
-  let cell = document.getElementsByClassName('cell')
-  for (let i = 0; i < gridLength * gridLength; i++) {
-      cell[i].style.backgroundColor = "white";
-  }
-} 
-
-const clearBtn = document.querySelector('.clearBtn');
-clearBtn.addEventListener('click', function() {
-  clearGrid();
-});
